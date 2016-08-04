b03137a58768e6fa3e8d229755f5f8e19b8aa1f6
apokalyptik <apokalyptik@apokalyptik.com>
2016-08-04 17:45:08 +0000

    starting to put things together...

--- dev/src/welcome.js
+++ dev/src/welcome.js
@@ -1,26 +1,11 @@
-import {inject} from 'aurelia-framework';
 import {bindable} from "aurelia-framework";
-import {Client} from 'api.js';
+import {User} from 'auth.js';
 
 export class Hello {
- 
-        @bindable loginPending=1;
-        @bindable loginAnonymous=0;
-        @bindable loginAuthenticated=0;
-
-        constructor(http) {
-                Client.fetch("ping")
-                        .then(data => {
-                                if ( data.status > 299 || data.status < 200 ) {
-                                        this.loginAnonymous = 1;
-                                } else {
-                                        this.loginAuthenticated = 1;
-                                }
-                                this.loginPending = 0;
-                        }).bind(this);
-        }
-
-        sayHello() {
-        alert(`Hello ${this.firstName} ${this.lastName}. Nice to meet you.`);
+        constructor() {
+                this.loggedIn = false;
+                this.loggedOut = false;
+                this.loginPending = true;
+                this.user = new User();
         }
 }
