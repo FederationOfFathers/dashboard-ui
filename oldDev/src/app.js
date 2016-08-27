925c5c462c603601af7a6f2154cba80b0ee1bbbe
Demitrious Kelly <demitrious@automattic.com>
2016-08-27 10:13:11 -0700

    version control means we do not have to keep old things around. they will be available in the repo history after being deleted...

--- oldDev/src/app.js
+++
@@ -1,9 +0,0 @@
-export class App {
-  configureRouter(config, router) {
-    config.title = 'FoF Dashboard';
-    config.map([
-      { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: true, title: 'Welcome' }
-    ]);
-    this.router = router;
-  }
-}
