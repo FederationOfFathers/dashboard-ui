23698612c81983ef6f562448e5bb4fd5cbd0591c
TheAndyBob <TheAndyBob@me.com>
2016-08-29 07:56:19 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	dashboard/scripts/app-bundle.js
    #	dashboard/scripts/app-bundle.js.map
    #	dashboard/scripts/vendor-bundle.js
    #	dashboard/src/app.js

--- dashboard/src/admin/admin.js
+++ dashboard/src/admin/admin.js
@@ -2,8 +2,19 @@ import {inject} from 'aurelia-framework';
 import {UserCache} from 'cache/user';
 
 @inject(UserCache)
-export class AdminPortal{
+export class Admin{
     constructor(userCache){
-        this.userCache = userCache;
+        this.userCache = userCache;  
+        this.user = this.userCache.get();     
+    }
+    activate(){
+       
+    }
+
+    get isAdmin(){
+        return this.user.admin;
+    }
+    get stringifiedUser(){
+        return JSON.stringify(this.user.user);
     }
 }
