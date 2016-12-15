23f4fb021513ffcdea68eb263fd14a910348353c
TheAndyBob <TheAndyBob@me.com>
2016-12-15 08:07:46 -0500

    First draft of racing stats

--- src/team/stats/racing/racing.js
+++ src/team/stats/racing/racing.js
@@ -1,28 +1,35 @@
-import {inject} from 'aurelia-framework';
+import {inject, computedFrom} from 'aurelia-framework';
 import {RacerCache} from 'team/stats/racing/racerCache';
+import {StatsMagic} from 'team/stats/statsMagic';
 
-@inject(RacerCache)
+@inject(RacerCache, StatsMagic)
 export class RacingStats {
 
-    constructor(racerCache){
+    constructor(racerCache, statsMagic){
         this._racerCache = racerCache;
+        this._statsMagic = statsMagic;
 
-        this.tracks = [];
 
-        for(let i = 0; i < 5; i++){
-            this.tracks.push({
-                name: `Track ${i}`
-            })
-        }
+        this.tracks = [];   
+        this._racers = [];    
 
     }
 
     activate() {
         this.selectedTrack = this.tracks[0];
+        return this._statsMagic.initialize()
+            .then(()=> {
+                this._racers = this._racerCache.getRacerStats();
+                this.tracks = this._racerCache.getTracks().sort((a, b) => a.toLowerCase() > b.toLowerCase() ? 1 : -1);
+            });
+    
     }
 
+    @computedFrom('selectedTrack')
     get racers(){
-        return this._racerCache.getRacersByTrack(this.selectedTrack);
+        return this._racers
+            .filter(r => r.track === this.selectedTrack)
+            .sort((a, b) => parseInt(a.time) < parseInt(b.time) ? -1 : 1);
     }
 
 }
---
+++ src/team/stats/statsMagic.js
@@ -0,0 +1,88 @@
+import {inject} from 'aurelia-framework';
+import {StatsApi} from 'api/stats';
+
+const STAT_NAMES = { //TODO: move this to some external file
+    'srl' : [130006,130007,130008,130009]
+}
+
+@inject(StatsApi)
+export class StatsMagic {
+    constructor(statsApi){
+        this._statsApi = statsApi;
+        this.categories = new Map();
+        this.destinyStatCategories = [];
+        this.destinyStats;
+    }
+
+    initialize(){
+        this.categories = new Map();
+        return this._statsApi.getCategories()
+            .then(c => {
+                for(let category of c){
+                    this.categories.set(category.ID, category);
+                }
+                this.destinyStatCategories = this.getStatSummaryByProduct('destiny');
+                return this.getAllDestinyStats();
+            })
+    }
+
+    getStatSummaryByProduct(product){
+        let result = [];
+        this.categories.forEach((value, key) => {
+            if(value.Product == product){
+                result.push(value);
+            }
+        });
+    
+        return result;
+    }
+
+    getAllDestinyStats(){
+        //TODO: Optimize the data object to make a smaller footprint
+        let statsToGet = [];
+        this.destinyStatCategories.forEach((value, key)=>{
+            statsToGet.push(value.ID);
+        });
+        return this._statsApi.getById(statsToGet.join(','))
+            .then(stats =>{
+                this.transformStats(stats);
+                this.destinyStats = stats;
+                return this.destinyStats;
+            })
+    }
+    getStatByStatName(name){
+        //TODO: Need Stat Name lookup for id
+        let result = [];
+        for(let userID in this.destinyStats){
+            let userStat = this.destinyStats[userID];
+            switch(name) {
+                case 'srl':
+                    let srlStatIDs = STAT_NAMES.srl;
+                    for(let i = 0; i < srlStatIDs.length; i++){
+                        let stat = userStat[srlStatIDs[i]];
+                        if(stat){
+                            result.push(Object.assign({UserID: userID}, stat));
+                        }
+                    }
+                    break;
+                default: 
+                    break;
+            }
+        }
+        return result;
+    }
+    getStatById(id){
+
+    }
+
+    transformStats(stats){
+        for(let userID in stats){
+            let userStat = stats[userID];
+            for(let stat in userStat){
+                let statValue = userStat[stat];
+                let prettyStatName = Object.assign({Value: statValue}, this.categories.get(parseInt(stat)));
+                userStat[stat] = prettyStatName;
+            }
+        }
+    }
+}
