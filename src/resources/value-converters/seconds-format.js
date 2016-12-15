4b53d045417c913287c3e90f25abf3167f34a2a8
TheAndyBob <TheAndyBob@me.com>
2016-12-15 13:09:11 -0500

    tweak millisecond timing to be more accurate

--- src/resources/value-converters/seconds-format.js
+++ src/resources/value-converters/seconds-format.js
@@ -4,8 +4,8 @@ export class SecondsFormatValueConverter {
         let totalSeconds = time / 1000;
         let minutes = Math.floor(totalSeconds / 60);
         let seconds = Math.floor(totalSeconds - (minutes * 60));
-        let milliseconds = ((totalSeconds - seconds - (minutes * 60).toFixed(2) + "0")).substring(2, 4); //adding "0" to make string for trimming
+        let milliseconds = ((totalSeconds - seconds - (minutes * 60) + "0")).substring(2, 6); //adding "0" to make string for trimming
 
-        return ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2) + ":" + ("0" + milliseconds).slice(-2);
+        return ("0" + minutes).slice(-2) + ":" + ("0" + seconds).slice(-2) + ":" +  milliseconds;
     }
 }
