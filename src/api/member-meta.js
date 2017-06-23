fdfdee0ddeb0ee0cf52c135835ab9b33114a287f
Benjamin Cherian <benjamin.cherian@3sharecorp.com>
2017-06-23 12:56:40 -0400

    Adding member meta api

---
+++ src/api/member-meta.js
@@ -0,0 +1,28 @@
+import {inject} from 'aurelia-framework';
+import {Api} from './api';
+
+@inject(Api)
+export class MemberMetaApi {
+
+    constructor(api){
+        this.api = api;
+    }
+
+    get(slackID, metaKey){
+        if(slackID && metaKey){
+            return this.api.get("meta/member/" + slackID + "/" + metaKey);
+        } else if (slackID){
+            return this.api.get("meta/member/" + slackID);
+        } else {
+            return null;
+        }
+    }
+
+    delete(slackID, metaKey){
+        return this.api.delete("meta/member/" + slackID + "/" + metaKey);
+    }
+
+    set(slackID, metaData){
+        return this.api.put("meta/member/" + slackID, metaData);
+    }
+}
